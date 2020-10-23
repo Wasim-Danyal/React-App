25845335d24b9a60823c1378774c8f692ce77cbd
Wasim-Danyal <wasim.danyal1@gmail.com>
2020-10-23 11:50:14 +0100

    Testing cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react'
 import './Cards.css';
-import Card from 'react-bootstrap/Card'
+import { Card, Button, Grid } from "react-bootstrap";
 
 export default class Cards extends Component {
     render() {
         return (
             <div className='cards'>
       <h1>Adventure starts here</h1>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="https://www.thefactsite.com/wp-content/uploads/2017/02/top-ten-travel-destinations.jpg" />
-        <Card.Body>
-          <Card.Title>Destinations</Card.Title>
-          <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-      </Card.Text>
+      <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src="" />
+      <Card.Body>
+        <Card.Title>Lorem Ipsum</Card.Title>
+        <Card.Text>
+          Lorem Ipsum
+        </Card.Text>
+        <Button variant="primary">Github</Button>
       </Card.Body>
-      </Card>
+    </Card>
+
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src="" />
+      <Card.Body>
+        <Card.Title>Lorem Ipsum</Card.Title>
+        <Card.Text>
+          Lorem Ipsum        
+        </Card.Text>
+        <Button variant="primary">Github</Button>
+      </Card.Body>
+    </Card>
+
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src="" />
+      <Card.Body>
+        <Card.Title>Lorem Ipsum</Card.Title>
+        <Card.Text>
+        Lorem Ipsum
+        </Card.Text>
+        <Button variant="primary">Github</Button>
+      </Card.Body>
+    </Card>
         </div>
 
     )
